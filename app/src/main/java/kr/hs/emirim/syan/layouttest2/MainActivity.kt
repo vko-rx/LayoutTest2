a53bba6ca2af897b0c87a175e3278519f55d538d
vko-rx <amhdksalsgnl@gmail.com>
2023-05-30 19:38:11 +0900

    코틀린 클래스에서 Button 생성

--- app/src/main/java/kr/hs/emirim/syan/layouttest2/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/syan/layouttest2/MainActivity.kt
@@ -3,7 +3,9 @@ package kr.hs.emirim.syan.layouttest2
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +17,14 @@ class MainActivity : AppCompatActivity() {
         var params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
             LinearLayout.LayoutParams.MATCH_PARENT)
         setContentView(linear1, params)
+
+        var btn = Button(this)
+        btn.text = "클릭해 보세요"
+        btn.setBackgroundColor(Color.rgb(255, 0, 255))
+        linear1.addView(btn)
+
+        btn.setOnClickListener{
+            Toast.makeText(applicationContext, "클래스에서 생성된 버튼입니다", Toast.LENGTH_SHORT).show()
+        }
     }
 }
